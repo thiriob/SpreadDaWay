01499fd14c23fcb4381bdbeb533b3dd991af69d7
Unknown <florent.baris@epitech.eu>
2018-01-27 14:54:03 +0100

    Queen fixed

--- Assets/movements/QueenController.cs
+++ Assets/movements/QueenController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class QueenController : MonoBehaviour {
 
     public SceneLoader map;
-
-    public float speed = 5.0f;
+    float sizeMapDiv2x;
+    float sizeMapDiv2y;
+    public float speed = 2.0f;
 	// Use this for initialization
 	void Start () {
-		
-	}
+	    sizeMapDiv2x = map.Size.x / 2.0f;
+	    sizeMapDiv2y = map.Size.y / 2.0f;
+    }
 
 	// Update is called once per frame
     void FixedUpdate () {
@@ -18,11 +20,11 @@ public class QueenController : MonoBehaviour {
         /* Left/right/up/down movements */
         Vector2 movementVector = new Vector2();
 
-        movementVector += Vector2.right * Input.GetAxis("Horizontal");
-        movementVector += Vector2.up * Input.GetAxis("Vertical");
+        movementVector += Vector2.right * Input.GetAxisRaw("Horizontal");
+        movementVector += Vector2.up * Input.GetAxisRaw("Vertical");
+        if (movementVector.x == 0 || movementVector.y == 0)
+            movementVector *= 1.41f;
         movementVector *= speed * Time.deltaTime;
-        float sizeMapDiv2x = map.Size.x / 2.0f;
-        float sizeMapDiv2y = map.Size.y / 2.0f;
         float queenXPos = transform.position.x + movementVector.x;
         float queenYPos = transform.position.y + movementVector.y;
         if (queenXPos > map.Center.x + sizeMapDiv2x || queenXPos < map.Center.x - sizeMapDiv2x) {
