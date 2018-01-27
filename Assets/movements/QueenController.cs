9612a5292ddd52dee16f6ca35d83b7467c89bc35
Vincent PICOT <vincent.picot.57@gmail.com>
2018-01-27 10:20:41 +0100

    Try to stop queen from going out of the map

--- Assets/movements/QueenController.cs
+++ Assets/movements/QueenController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class QueenController : MonoBehaviour {
 
+    public SceneLoader map;
+
     public float speed = 5.0f;
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,22 @@ public class QueenController : MonoBehaviour {
         /* Left/right/up/down movements */
         var movementVector = Vector2.right * Input.GetAxis("Horizontal");
         movementVector += Vector2.up * Input.GetAxis("Vertical");
-        transform.Translate(movementVector * speed * Time.deltaTime);
+        movementVector *= speed * Time.deltaTime;
+        float sizeMapDiv2x = map.Size.x / 2.0f;
+        float sizeMapDiv2y = map.Size.y / 2.0f;
+        if (movementVector.x > map.Center.x + sizeMapDiv2x) {
+            movementVector.x = map.Center.x + sizeMapDiv2x;
+        }
+        if (movementVector.x < map.Center.x - sizeMapDiv2x) {
+            movementVector.x = map.Center.x - sizeMapDiv2x;
+        }
+        if (movementVector.y > map.Center.y + sizeMapDiv2y) {
+            movementVector.y = map.Center.y + sizeMapDiv2y;
+        }
+        if (movementVector.y < map.Center.y - sizeMapDiv2y) {
+            movementVector.y = map.Center.y - sizeMapDiv2y;
+        }
+        transform.Translate(movementVector);
 
         /* Mouse related movements */
         var mousePosX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
